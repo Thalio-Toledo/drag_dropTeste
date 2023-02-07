ac5e778779e10ff2b71d0a4f067e9a0976156d2b
Thalio-Toledo <thalio.speed@hotmail.com>
2023-02-07 20:49:24 -0300

    Ajuste

--- src/app/drag-drop/drag-drop.component.ts
+++ src/app/drag-drop/drag-drop.component.ts
@@ -6,26 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./drag-drop.component.css']
 })
 export class DragDropComponent {
-  list1:string[]= ['tsk1', 'tsk2']
-  list2:string[]= ['toDo1', 'toDo2']
+  list1: string[] = ['tsk1', 'tsk2']
+  list2: string[] = ['toDo1', 'toDo2']
 
 
-  dragCard(lista:string[], card:string){
-    console.log('começando...')
-    console.log(lista)
+  dragStart(listaAntiga: string[], card: string) {
+    console.log(listaAntiga)
     console.log(card)
+    return listaAntiga
   }
 
-  dragEndCard(card:string){
-    console.log('parando...')
-    console.log(card)
-    this.list2.splice(this.list2.indexOf(card),1)
-    //list.splice(list.indexOf('foo'), 1);
-    this.list1.push(card)
+  
+  dragOver(listaNova:string[]) {
+    console.log(listaNova)
+    return listaNova
   }
 
-  dragOverDrop(event:object){
-    console.log('Dropable over...')
-    console.log(event)
+  dragEnd(listaAntiga:string[],listaNova:string[],card:string) {
+    // console.log('parando...')
+    // console.log(card)
+    listaAntiga.splice(listaAntiga.indexOf(card),1)
+    listaNova.push(card)
+
+    // this.list2.splice(this.list2.indexOf(card), 1)
+    // //list.splice(list.indexOf('foo'), 1);
+    // this.list1.push(card)
   }
 }
